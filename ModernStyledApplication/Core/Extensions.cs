41605c184025b340915b5cdc834114741ec0b7c2
daviimuse <musitellidavideluigi2003@gmail.com>
2022-10-22 13:41:04 +0200

    Adding Siderbar buttons

---
+++ ModernStyledApplication/Core/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ModernStyledApplication.Core
+{
+    internal class Extensions
+    {
+        public static readonly DependencyProperty Icon = 
+            DependencyProperty.RegisterAttached("Icon", typeof(string), typeof(Extensions), new PropertyMetadata(default(string)));
+
+        public static void SetIcon(UIElement element, string value)
+        { 
+            element.SetValue(Icon, value);  
+        }
+
+        public static string GetIcon(UIElement element)
+        {
+            return (string)element.GetValue(Icon);
+        }
+    }
+}
